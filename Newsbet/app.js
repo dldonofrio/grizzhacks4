a03b492a562a42fef8ee8b24273f971d5664aca3
NasSharaf <nsrsharaf7@gmail.com>
2019-09-28 17:30:54 -0400

    Please delete YelpCamp, I'm sorry

---
+++ Newsbet/app.js
@@ -0,0 +1,23 @@
+// Starting import libraries
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("assets"));
+
+var request = require("request");
+
+var bodyParser = require("body-parser");
+
+var mongoose = require("mongoose");
+
+mongoose.connect("");
+
+app.set("view engine", 'ejs');
+
+
+
+// port listening stuff
+app.listen(8000, function() {
+    console.log("GOd is dead, and we have killed him");
+});

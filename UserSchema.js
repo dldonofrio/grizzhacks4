d10319dfbe057c22fa6b0eeb6939401c9eb19fa1
dsheridan2 <dsheridan2@gmail.com>
2019-09-28 19:37:15 -0400

    Added user schema

---
+++ UserSchema.js
@@ -0,0 +1,16 @@
+var mongoose                 = require("mongoose");
+var passportLocalMonggose = require("passport-local-mongoose");
+
+var UserSchema = new mongoose.Schema({
+    username:  String,
+    password:  String,
+    balance:   Number,
+    amntWon:   Number,
+    amntLose:  Number,
+    totalWins: Number,
+    totalLose: Number,
+});
+
+UserSchema.plugin(passportLocalMonggose);
+
+module.exports = mongoose.model("User", UserSchema);
